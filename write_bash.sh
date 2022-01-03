d0975c7d9458d3374e005c28087c1a70e81db4b0
Romain <romain.panno@epitech.eu>
2022-01-03 17:54:54 +0100

    [FIX] Makefile, zshrc, bashrc alias

--- write_bash.sh
+++ write_bash.sh
@@ -6,12 +6,6 @@
 ## init_repo
 ##
 
-GREEN="\x1b[32;01m["
-UNGREEN="]\x1b[0m"
-
-FLASHING="\e[6m"
-UNFLASHING="\e0"
-
 if [ -f ~/.zshrc ]; then
     echo 'alias liber="bash ~/liber/init_repo.sh"' >> ~/.zshrc
 else
