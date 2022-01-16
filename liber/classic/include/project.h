efd3b4b2eab936a8a4563d45fd983c9c1c35f462
Romain <romain.panno@epitech.eu>
2022-01-16 13:21:35 +0100

    Fix norm with '#' indentation

--- liber/classic/include/project.h
+++ liber/classic/include/project.h
@@ -7,6 +7,6 @@
 
 #ifndef $HEADER_NAME_H_
     #define $HEADER_NAME_H_
-#define EXIT_EPITECH 84
+    #define EXIT_EPITECH 84
 
 #endif /* !$HEADER_NAME_H_ */
--- liber/csfml-without-lib/include/project.h
+++ liber/csfml-without-lib/include/project.h
@@ -7,10 +7,10 @@
 
 #ifndef $HEADER_NAME_H_
     #define $HEADER_NAME_H_
-#include <SFML/Graphics.h>
-#include <SFML/Window.h>
-#include <SFML/Audio.h>
+    #include <SFML/Graphics.h>
+    #include <SFML/Window.h>
+    #include <SFML/Audio.h>
 
-#define EXIT_EPITECH 84
+    #define EXIT_EPITECH 84
 
 #endif /* !$HEADER_NAME_H_ */
