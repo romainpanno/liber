51c311b2220d4211f5abd8e553a0861db1700956
Romain <romain.panno@epitech.eu>
2022-02-07 14:13:19 +0100

    TRY choose lib or not

--- liber.sh
+++ liber.sh
@@ -206,7 +206,6 @@ elif [[ $1 ]] && [[ $2 ]]; then
         print_error
     fi
 elif [ $1 ]; then
-    echo -e "liberpath = $LIB"
     if [ $1 == "-u" ]; then
         update
     elif [[ $1 == "-wl" || $1 == "--without-lib" ]] || [[ $IS_NOLIB == "YES" ]]; then
